c2da2cbf946447f1205f52774a9babaa940c098a
Marie Carrere <marie.carrere@icloud.com>
2019-01-13 00:09:20 +0100

    initial commit 🚀

---
+++ src/components/Weather.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Weather extends Component {
+    render() {
+        return (
+            <div>Weather component</div>
+        );
+    }
+}
+
+export default Weather;
