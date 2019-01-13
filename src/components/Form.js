ed660fa273e6f1a1e38a1c4d02e2b1845bb0f5a7
Marie Carrere <marie.carrere@icloud.com>
2019-01-13 15:55:32 +0100

    weather component working fine

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 class Form extends Component {
     render() {
         return (
-            <div>Form component</div>
+            <form onSubmit={this.props.getWeather}>
+                <input type="text" name="city" placeholder="city..."/>
+                <input type="text" name="country" placeholder="country..."/>
+                <button>Get Weather</button>
+            </form>
         );
     }
 }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,7 +3,12 @@ import React, { Component } from 'react';
 class Weather extends Component {
     render() {
         return (
-            <div>Weather component</div>
+            <div>
+                { this.props.city && this.props.country && <p>Location: {this.props.city}, {this.props.country}</p> }
+                { this.props.temperature && <p>Temperature: {this.props.temperature}</p> }
+                { this.props.humidity && <p>Humidity: {this.props.humidity}</p> }
+                {this.props.description && <p>Conditions: {this.props.description}</p> }
+            </div>
         );
     }
 }
