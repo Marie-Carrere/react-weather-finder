d16e7ce42492a9a548d141d3dd7c3331cc8ead5d
marie-carrere <marie.carrere@icloud.com>
2019-01-13 18:31:08 +0100

    components done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 const Form = props => (
-    <form onSubmit={ props.getWeather }>
-        <input type="text" name="city" placeholder="city..." />
-        <input type="text" name="country" placeholder="country..." />
+    <form onSubmit={props.getWeather}>
+        <input type="text" name="city" placeholder="City..." />
+        <input type="text" name="country" placeholder="Country..." />
         <button>Get Weather</button>
     </form>
 );
